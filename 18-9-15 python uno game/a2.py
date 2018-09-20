ea3494b7b0084209bedfe8c81b7ed69375a5b6b0
LiuYinCarl <1427518212@qq.com>
2018-09-20 20:44:13 +0800

    完成UNO项目

--- 18-9-15 python uno game/a2.py
+++ 18-9-15 python uno game/a2.py
@@ -1,62 +1,90 @@
+#!/usr/bin/env python3
+"""
+Assignment 2 - UNO++
+CSSE1001/7030
+Semester 2, 2018
+"""
+
 from random import shuffle
+
+__author__ = "Your name & student number here"
 
 
 class Card(object):
+    """
+    The basic type of colour and number.
+    """
     def __init__(self, number, colour):
-        """
-        initiate Card
-        :param number: the number of card
-        :param colour: the color of card
-        """
         self.number = number
         self.colour = colour
-        self.pickup_amount = 0  # pick up amount of this card
-        self.skip = False       # If this card is a skip card
-        self.reverse = False    # If this card is a reverse card
+        self.pickup_amount = 0
+        self.skip = False
+        self.reverse = False
 
     def __str__(self):
-        return 'Card({},{})'.format(self.number, self.colour)
+        return 'Card({}, {})'.format(self.number, self.colour)
 
     def __repr__(self):
-        return 'Card({},{})'.format(self.number, self.colour)
+        return 'Card({}, {})'.format(self.number, self.colour)
 
     def get_number(self):
+        """
+        Returns the card number
+        """
         return self.number
 
     def get_colour(self):
+        """
+        Returns the card colour
+        """
         return self.colour
 
     def set_number(self, number):
-        if isinstance(number, int):
-            self.number = number
-        else:
-            raise TypeError(
-                "number is not int Type"
-            )
+        """
+        Set the number value of the card
+        """
+        self.number = number
 
     def set_colour(self, colour):
-        if isinstance(colour, str):
-            self.colour = colour
-        else:
-            raise TypeError(
-                "colour is not str Type"
-            )
+        """
+        Set the colour of the card
+        """
+        self.colour = colour
 
     def get_pickup_amount(self):
+        """
+        Returns the amount of cards the next player should pickup
+        """
         return self.pickup_amount
 
     def matches(self, card):
         """
-       Return True if the other card is playable on top of this card,
-       otherwise return False
-       """
-        return (
-                self.colour == card.colour or
-                self.number == card.number
-        )
+        matches(self, card: Card): Determines if the next
+        card to be placed on the pile matches this card.
+        """
+        # select out the special cards
+        if self.pickup_amount == 4:
+            return True
+        elif self.get_colour() == card.get_colour() or self.get_number() == card.get_number():
+            return True
+        else:
+            return False
 
     def play(self, player, game):
-        pass
+        """
+        Perform a special card action. The base Cards has no special action
+        """
+        if self.pickup_amount == 2:
+            cards = game.pickup_pile.pick(2)
+            game.get_turns().peak().get_deck().add_cards(cards)
+            # player.deck.add_cards(cards)
+        elif self.pickup_amount == 4:
+            cards = game.pickup_pile.pick(4)
+            game.get_turns().peak().get_deck().add_cards(cards)
+        elif self.skip:
+            game.skip()
+        elif self.reverse:
+            game.reverse()
 
 
 class SkipCard(Card):
@@ -68,25 +96,26 @@ class SkipCard(Card):
         self.skip = True
 
     def __str__(self):
-        return 'SkipCard({},{})'.format(self.number, self.colour)
+        return 'SkipCard({}, {})'.format(self.number, self.colour)
 
     def __repr__(self):
-        return 'SkipCard({},{})'.format(self.number, self.colour)
+        return 'SkipCard({}, {})'.format(self.number, self.colour)
 
 
 class ReverseCard(Card):
     """
     Initiate ReverseCard, make self.reverse True
     """
+
     def __init__(self, number, colour):
         super().__init__(number, colour)
         self.reverse = True
 
     def __str__(self):
-        return 'ReverseCard({},{})'.format(self.number, self.colour)
+        return 'ReverseCard({}, {})'.format(self.number, self.colour)
 
     def __repr__(self):
-        return 'ReverseCard({},{})'.format(self.number, self.colour)
+        return 'ReverseCard({}, {})'.format(self.number, self.colour)
 
 
 class Pickup2Card(Card):
@@ -98,10 +127,10 @@ class Pickup2Card(Card):
         self.pickup_amount = 2
 
     def __str__(self):
-        return 'Pickup2Card({},{})'.format(self.number, self.colour)
+        return 'Pickup2Card({}, {})'.format(self.number, self.colour)
 
     def __repr__(self):
-        return 'Pickup2Card({},{})'.format(self.number, self.colour)
+        return 'Pickup2Card({}, {})'.format(self.number, self.colour)
 
 
 class Pickup4Card(Card):
@@ -113,83 +142,116 @@ class Pickup4Card(Card):
         self.pickup_amount = 4
 
     def __str__(self):
-        return 'Pickup4Card({},{})'.format(self.number, self.colour)
+        return 'Pickup4Card({}, {})'.format(self.number, self.colour)
 
     def __repr__(self):
-        return 'Pickup4Card({},{})'.format(self.number, self.colour)
+        return 'Pickup4Card({}, {})'.format(self.number, self.colour)
 
 
 class Deck(object):
-
-    def __init__(self, cards=[]):
-        self.cards = cards
+    """
+    A collection of ordered Uno cards.
+    """
+    def __init__(self, starting_cards=None):
+        self.cards = []
+        if not starting_cards:
+            pass
+        elif isinstance(starting_cards, list):
+            self.cards = starting_cards
+        else:
+            self.cards.append(starting_cards)
 
     def get_cards(self):
+        """
+        Returns a list of cards in the deck.
+        """
         return self.cards
 
     def get_amount(self):
+        """
+        Returns the amount of cards in a deck.
+        """
         return len(self.cards)
 
     def shuffle(self):
+        """
+        Shuffle the order of the cards in the deck.
+        """
         shuffle(self.cards)
 
     def pick(self, amount=1):
-        if isinstance(amount, int):
-            pick_cards = self.cards[-amount:]
-            del (self.cards[-amount:])
-            pick_cards.reverse()
+        """
+        Take the first 'amount' of cards off the deck and return them.
+        """
+        if len(self.cards) >= amount:
+            pick_cards = []
+            for i in range(amount):
+                pick_cards.append(self.cards[-1])
+                del(self.cards[-1])
             return pick_cards
         else:
-            raise TypeError(
-                "amount is not int Type"
-            )
+            # don't have enough cards
+            return []
+
+    def add_card(self, card):
+        """
+        Place a list of cards on top of the deck.
+        """
+        self.cards.append(card)
 
     def add_cards(self, cards):
-        if isinstance(cards, Card):
-            self.cards.append(cards)
-        elif isinstance(cards, tuple) or isinstance(cards, list):
-            for card in cards:
-                self.cards.append(card)
-        else:
-            raise TypeError(
-                "don't know the type of cards"
-            )
+        """
+        Place a list of cards on top of the deck.
+        """
+        for card in cards:
+            self.cards.append(card)
 
     def top(self):
         """
-        >>> deck.get_cards()
-        [Card(12, red), Pickup2Card(0, red), ReverseCard(0, blue)]
-        >>> deck.top()
-        ReverseCard(0, blue)
+        Peaks at the card on top of the deck and
+        returns it or None if the deck is empty.
         """
-        return self.cards[-1]
+        if len(self.cards) >= 1:
+            return self.cards[-1]
+        else:
+            print("the deck is empty")
 
 
 class Player(object):
-
+    """
+    The base type of player which is not meant to be initiated (i.e. an abstract class)..
+    """
     def __init__(self, name):
-        """
-        Initiate Player
-        :param name: name of Player
-        """
         self.name = name
-        self.deck = None
+        self.deck = Deck()
         self._win = False
 
     def get_name(self):
+        """
+        Returns the name of the player.
+        """
         return self.name
 
     def get_deck(self):
-        self.deck = Deck()
+        """
+        Returns the players deck of cards.
+        """
         return self.deck
 
     def is_playable(self):
+        """
+        Returns True iff the players moves aren't automatic.
+        """
         raise NotImplementedError(
             "is_playable to be implemented by subclasses"
         )
 
     def has_won(self):
-        if not len(self.deck.get_cards()):
+        """
+        Returns True if the player
+        has an empty deck and has therefore won.
+        """
+        if not self.deck.get_amount():
             self._win = True
         else:
             self._win = False
@@ -197,24 +259,65 @@ class Player(object):
 
     def pick_card(self, putdown_pile):
         """
-        pick_card(self, putdown_pile: Deck): Selects a card to play from the players current deck.
-        :param putdown_pile: a deck
-        :return: card to play
+        Selects a card to play from the players current deck.
         """
-        card = putdown_pile.pick()
-        if not card:
-            return None
-        else:
-            return card
+        raise NotImplementedError(
+            "is_playable to be implemented by subclasses"
+        )
 
 
 class HumanPlayer(Player):
+    """
+    A human player that selects cards to play using the GUI.
+    """
+    def __init__(self, name):
+        super().__init__(name)
 
     def is_playable(self):
         return True
 
+    def pick_card(self, putdown_pile):
+        """
+        Selects a card to play from the players current deck.
+        """
+        for card in self.deck.get_cards():
+            if card.colour == putdown_pile.top().colour and card.number == putdown_pile.top().number:
+                return None
+        if len(self.deck.get_cards()) > 0:
+            for i, card in enumerate(self.deck.get_cards()):
+                if card.matches(putdown_pile.top()):
+                    need_card = card
+                    del self.deck.cards[i]
+                    return need_card
+        else:
+            return None
+
 
 class ComputerPlayer(Player):
+    """
+    A computer player that selects cards to play automatically.
+    """
+    def __init__(self, name):
+        super().__init__(name)
 
     def is_playable(self):
-        return None
+        return False
+
+    def pick_card(self, putdown_pile):
+        """
+        Selects a card to play from the players current deck.
+        """
+        for card in self.deck.get_cards():
+            if card.colour == putdown_pile.top().colour and card.number == putdown_pile.top().number:
+                return None
+        if len(self.deck.get_cards()) > 0:
+            for i, card in enumerate(self.deck.get_cards()):
+                if card.matches(putdown_pile.top()):
+                    need_card = card
+                    del self.deck.cards[i]
+                    return need_card
+        else:
+            return None
+
+
+
